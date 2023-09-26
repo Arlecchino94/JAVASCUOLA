d547789a16138e8072ef47b0068d784a6ad2179c
Khali <vetralla.alberto@hotmail.it>
2023-09-26 20:38:11 +0200

    Esercizi

--- src/main/java/edu/avogadro/Esercizio6Incremento.java
+++ src/main/java/edu/avogadro/Esercizio6Incremento.java
@@ -5,12 +5,13 @@ import javax.swing.*;
 public class Esercizio6Incremento {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog("Inserire la popolazione prima dei 2 anni");
-        double pop1 = Double.parseDouble(input);
+        int pop1 = Integer.parseInt(input);
         input = JOptionPane.showInputDialog("Inserire la popolazione dopo i 2 anni ");
-        double pop2 = Double.parseDouble(input);
-        double IncrEff = pop2-pop1;
+        int pop2 = Integer.parseInt(input);
+        int IncrEff = pop2-pop1;
         double IncrPerc = IncrEff*100.0/pop1;
-        JOptionPane.showMessageDialog(null, "L'incremento effettivo è: "+IncrEff+"\n"+
-                "La percentuale di incremento è del: "+IncrPerc+"%");
+        String output = String.format("La popolazione è aumentata di %d abitanti e in percentuale del %5.2f %%", IncrEff, IncrPerc);
+        JOptionPane.showMessageDialog(null, output);
+
     }
 }
