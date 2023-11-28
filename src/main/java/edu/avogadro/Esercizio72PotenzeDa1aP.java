e6c3fa7a36cae67843787fefbd2a12b372faa2a8
Khali <vetralla.alberto@hotmail.it>
2023-11-28 19:40:32 +0100

    Esercizi

--- src/main/java/edu/avogadro/Esercizio72PotenzeDa1aP.java
+++ src/main/java/edu/avogadro/Esercizio72PotenzeDa1aP.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Esercizio72PotenzeDa1aP {
     public static void main(String[] args) {
-        //Calcolare tutte le potenze da 1 a P di un numero N
+        //Calcolare tutte le potenze da UNO a P di un numero N
         String input = JOptionPane.showInputDialog("Inserire potenza");
         double base = Double.parseDouble(input);
         input = JOptionPane.showInputDialog("Inserire esponente");
@@ -14,6 +14,13 @@ public class Esercizio72PotenzeDa1aP {
         for (int i = 1; i <= esponente; i++) {
             potenza = potenza * base;
             output += (output.equals("")?"":",")+potenza;
+            /*
+            L'espressione (output.equals("")?"":",") è un operatore ternario e significa che tale espressione
+            può valere "" oppure ",", a seconda che il predicato prima del punto interrogativo valga vero o falso rispettivamente.
+            Pertanto se output.equals("") è vero (cioè se output è uguale alla stringa vuota) allora l'intera espressione vale "".
+            Se invece output.equals("") è falso (Cioè se output non è uguale alla stringa vuota) allora l'intera espressione
+            vale ",".
+             */
         }
         JOptionPane.showMessageDialog(null, output);
 
