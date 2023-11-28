50b8ee638c0e594d42727a72120489deefb50fc7
Khali <vetralla.alberto@hotmail.it>
2023-11-28 20:18:39 +0100

    Esercizi

--- src/main/java/edu/avogadro/Esercizio73.java
+++
@@ -1,7 +0,0 @@
-package edu.avogadro;
-
-public class Esercizio73 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/edu/avogadro/Esercizio73PotenzeDa1aN.java
@@ -0,0 +1,27 @@
+package edu.avogadro;
+
+import com.sun.nio.sctp.AbstractNotificationHandler;
+
+import javax.swing.*;
+
+public class Esercizio73PotenzeDa1aN {
+    public static void main(String[] args) {
+        //calcolare tutte le potenze da UNO a P dei valori da UNO a N
+        String input = JOptionPane.showInputDialog("Inserire colonne");
+        int n = Integer.parseInt(input);
+        input = JOptionPane.showInputDialog("Inserire riga");
+        int p = Integer.parseInt(input);
+        String outputGlobale = "";
+
+        for (int i = 1; i <= n; i++) {
+            int potenza = 1;
+            String output = "";
+            for (int j = 1; j <= p; j++) {
+                potenza = potenza * i;
+                output += (output.equals("")?"":",")+ String.format("%4d", potenza);
+            }
+            outputGlobale += (output.equals("")?"":"\n")+output;
+        }
+        JOptionPane.showMessageDialog(null, outputGlobale);
+    }
+}
