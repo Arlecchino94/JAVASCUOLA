e1b4daa210cab51784d46bd44a0221a4dbf357d4
Khali <vetralla.alberto@hotmail.it>
2023-12-05 19:16:34 +0100

    Esercizi

---
+++ src/main/java/edu/avogadro/Esercizio84.java
@@ -0,0 +1,19 @@
+package edu.avogadro;
+
+import javax.swing.*;
+
+public class Esercizio84 {
+    public static void main(String[] args) {
+        //Calcolare la somma di tutti i numeri dispari da uno a n
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Inserire un numero"));
+        int somma = 0;
+        for (int i = 1; i <=n; i++) {
+            if (i % 2 == 0) {
+                somma += i*i*i;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "La somma è: " + somma);
+
+    }
+}
+
