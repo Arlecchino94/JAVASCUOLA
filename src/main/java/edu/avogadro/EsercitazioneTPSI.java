5aad9123bbce05fbfb5e0d7bf3c6bc2d7c837821
Khali <vetralla.alberto@hotmail.it>
2024-01-09 20:49:41 +0100

    Esercizi

--- src/main/java/edu/avogadro/EsercitazioneTPSI.java
+++ src/main/java/edu/avogadro/EsercitazioneTPSI.java
@@ -1,14 +1,44 @@
 package edu.avogadro;
 
+
 import javax.swing.*;
-import java.util.Random;
+
 
 
 public class EsercitazioneTPSI {
 
+
     public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog("Inserisci un numero da indovinare");
-        int n = Integer.parseInt(input);
+        int min = 1;
+        int max = 1000;
+        int media = (min+max)/2;
+        String output = "";
+        output = "Inizio";
+        String input = JOptionPane.showInputDialog("Vuoi iniziare?");
+        while (input.equals("si")) {
+            String inputt = JOptionPane.showInputDialog("Il numero è maggiore o minore di: "+media);
+            if (inputt.equals("maggiore")) {
+                min = media +1;
+                media = (min+max)/2;
+            }
+            if (inputt.equals("minore")) {
+                max = media -1;
+                media = (min+max)/2;
+            }
+            if (inputt.equals("uguale")) {
+                JOptionPane.showMessageDialog(null, "La riposta è:"+media);
+                input  = JOptionPane.showInputDialog("Vuoi ricominciare?");
+                min = 1;
+                max = 1000;
+                media = (min+max)/2;
+            }
+            if (media == 0) {
+                JOptionPane.showMessageDialog(null, "Non è un numero tra 1 e 1000");
+                break;
+            }
+
+
+        }
 
     }
 }
