a1115e4e887618003d5ca316b2bcc69512968093
Khali <vetralla.alberto@hotmail.it>
2024-02-06 19:26:17 +0100

    Esercizi

--- src/main/java/edu/avogadro/Esercizio100.java
+++ src/main/java/edu/avogadro/Esercizio100.java
@@ -9,6 +9,7 @@ public class Esercizio100 {
         for (int i = 0; somma < n; i++) {
             somma = somma + i;
         }
+        //ciclo somma triangolare
         if (somma > n) {
             JOptionPane.showMessageDialog(null, "Il numero non è triangolare");
         }else {
