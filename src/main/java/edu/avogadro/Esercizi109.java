03bfad1bf235196fa23de2e892e12b93cc1fdfe5
Khali <vetralla.alberto@hotmail.it>
2024-02-19 21:19:20 +0100

    Esercizi

--- src/main/java/edu/avogadro/Esercizi109.java
+++ src/main/java/edu/avogadro/Esercizi109.java
@@ -3,11 +3,23 @@ package edu.avogadro;
 import javax.swing.*;
 
 public class Esercizi109 {
+    // Calcolare i primi N numeri Primi
     public static void main(String[] args) {
         int n = Integer.parseInt(JOptionPane.showInputDialog("Inserire quanti numeri primi si vogliono visualizzare"));
-        String output = "";
-        for (int i = n;i <=n;i++) {
-
+        int num = 2;
+        int contPrimi = 0;
+        while (contPrimi < n){
+            int contDiv = 0;
+            for (int i = 1;i <= num;i++) {
+                if (num % i == 0) {
+                    contDiv = contDiv + 1;
+                }
+            }
+            if (contDiv == 2){
+                JOptionPane.showMessageDialog(null, num);
+                contPrimi = contPrimi + 1;
+            }
+            num = num +1;
         }
     }
 }
---
+++ src/main/java/edu/avogadro/Esercizio110.java
@@ -0,0 +1,7 @@
+package edu.avogadro;
+
+public class Esercizio110 {
+    public static void main(String[] args) {
+
+    }
+}
