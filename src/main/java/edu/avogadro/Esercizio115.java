6e460128dde3b8adadde77098bb4b099f725becf
Khali <vetralla.alberto@hotmail.it>
2024-02-20 21:15:17 +0100

    Esercizi

--- src/main/java/edu/avogadro/Esercizio115.java
+++ src/main/java/edu/avogadro/Esercizio115.java
@@ -1,21 +1,23 @@
 package edu.avogadro;
 
-
 import javax.swing.*;
 
-public class Esercizio114 {
+public class Esercizio115 {
     public static void main(String[] args) {
-        //Dato un numero, scomporlo in fattori primi
+        //Dato un numero, scrivere una sola volta tutti i suoi fattori primi
         int n = Integer.parseInt(JOptionPane.showInputDialog("Inserisci un numero"));
         int fatt = 1;
         int cont = 2;
+        int divprec = 0;
         String output = "";
-        while ( n > fatt) {
-
+        while (n > fatt) {
             if (n % cont == 0) {
                 n /= cont;
-                output += (output.equals("")?"":" * ")+cont;
-            }else {
+                if (divprec != cont) {
+                    output += (output.equals("") ? "" : ",") + cont;
+                }
+                divprec = cont;
+            } else {
                 cont++;
             }
         }
