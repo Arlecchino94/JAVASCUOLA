eb34226c8f13ce72666286e78eed656c0699922f
Khali <vetralla.alberto@hotmail.it>
2024-05-07 20:56:47 +0200

    Esercizi

---
+++ src/main/java/edu/avogadro/Matrice8.java
@@ -0,0 +1,12 @@
+package edu.avogadro;
+
+public class Matrice8 {
+    public static void main(String[] args) {
+        /*Leggere un vettore di N elementi e disporre i dati in una matrice di M righe procedendo per righe e lasciando vuoti
+        gli elementi eventualmente inutilizzati della matrice; calcolare il numero di colonne necessarie.
+         */
+
+        String output = "";
+
+    }
+}
