6cbe0eab8ade4bba4fccaee5e6038af206c4ce09
Khali <vetralla.alberto@hotmail.it>
2024-10-02 21:14:36 +0200

    Esercizi

---
+++ src/main/java/edu/avogadro/VetrallaTPSI.java
@@ -0,0 +1,40 @@
+package edu.avogadro;
+
+
+import java.util.Scanner;
+
+public class VetrallaTPSI {
+
+
+    public static void main(String[] args) {
+        Scanner leggi = new Scanner(System.in);
+        System.out.println("Il numero è: "+leggiIntero(leggi));
+    }
+
+
+
+
+    public static int leggiIntero(Scanner leggi) {
+
+        while (true) {
+            try {
+                Exception err = new Exception("Il numero non è compreso nei limiti");
+                System.out.println("Inserisci il limite minimo");
+                int min = leggi.nextInt();
+                System.out.println("Inserisci il limite massimo");
+                int max = leggi.nextInt();
+                System.out.println("Inserisci il numero");
+                int num = leggi.nextInt();
+                if (num < min || num > max) {
+                    throw err;
+                }else {
+                    return num;
+                }
+            }
+            catch (Exception err){
+                System.out.println(err);
+                leggi.nextLine();
+            }
+        }
+    }
+}
